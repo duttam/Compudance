feb402e7642926104c0205c92d3e42870dc47f11
Manas Dutta <manas.dutta@dfyoung.com>
2014-08-10 14:40:09 -0400

    add second String

--- Compudance/src/HelloWorldBean.java
+++ Compudance/src/HelloWorldBean.java
@@ -15,11 +15,12 @@ public class HelloWorldBean implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	private String string;
-
+	private String secondString;
 
 	@PostConstruct
 	public void init(){
 		string = "this is a sinple demo test";
+		secondString = "this is a second String";
 	}
 
 	public String getString() {
@@ -28,6 +29,14 @@ public class HelloWorldBean implements Serializable{
 
 	public void setString(String string) {
 		this.string = string;
+	}
+
+	public String getSecondString() {
+		return secondString;
+	}
+
+	public void setSecondString(String secondString) {
+		this.secondString = secondString;
 	}
 
 	
