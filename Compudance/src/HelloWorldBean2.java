6f79045cb44b03b7e87f05fd0de08e0605ce3670
Manas Dutta <manas.dutta@dfyoung.com>
2014-08-10 15:26:54 -0400

    SECOND HELLO WORLD BEAN COMMIT

---
+++ Compudance/src/HelloWorldBean2.java
@@ -0,0 +1,43 @@
+
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="helloWorld")
+@ViewScoped
+public class HelloWorldBean2 implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String string;
+	private String secondString;
+
+	@PostConstruct
+	public void init(){
+		string = "this is a sinple demo test";
+		secondString = "this is a second String";
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	public String getSecondString() {
+		return secondString;
+	}
+
+	public void setSecondString(String secondString) {
+		this.secondString = secondString;
+	}
+
+	
+}
